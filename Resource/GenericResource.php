b30826bf3b093d1d49dbdad9571d74c866e9e0ff
Taihao Zhang <jason@zth.me>
2012-10-29 01:36:45 +1100

    Implement relative path resolution.

--- Resource/GenericResource.php
+++ Resource/GenericResource.php
@@ -15,6 +15,12 @@ class GenericResource implements Resource
 
     /**
      * Creates a generic resource with a path.
+     *
+     * @param string $path
+     *   Normalized relative path.
+     *
+     * @throws \InvalidArgumentException
+     *   If the path is empty, absolute, or denormalized.
      */
     public function __construct($path)
     {
@@ -23,6 +29,9 @@ class GenericResource implements Resource
         }
         if ($path[0] == '/') {
             throw new \InvalidArgumentException('Path must not be absolute.');
+        }
+        if (strpos($path, '..') !== false) {
+            throw new \InvalidArgumentException('Path must be normalized.');
         }
         $this->path = rtrim($path, '/');
     }
@@ -33,5 +42,31 @@ class GenericResource implements Resource
     public function getPath()
     {
         return $this->path;
+    }
+
+    /**
+     * Resolves a relative path based on the path of this resource.
+     */
+    public function resolvePath($path)
+    {
+        if ($path[0] == '/') {
+            throw new \InvalidArgumentException('Cannot resolve absolute path.');
+        }
+
+        $segments = explode('/', $this->path);
+        $path = trim($path, '/');
+        foreach (explode('/', trim($path, '/')) as $segment) {
+            if ($segment == '.') {
+                continue;
+            }
+            elseif ($segment == '..' && !empty($segments)) {
+                array_pop($segments);
+            }
+            else {
+                $segments[] = $segment;
+            }
+        }
+
+        return implode('/', $segments);
     }
 }
