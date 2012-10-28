7029e02626fbc5f5adcd3de422da8d503644fee4
Taihao Zhang <jason@zth.me>
2012-10-28 23:10:47 +1100

    Test directory.

--- Tests/ResourceMirrorTest.php
+++ Tests/ResourceMirrorTest.php
@@ -12,7 +12,7 @@ class ResourceMirrorTest extends \PHPUnit_Framework_TestCase
      */
     public function testCreate()
     {
-        $mirror = new ResourceMirror(new EventDispatcher(), 'http://example.com/');
+        $mirror = new ResourceMirror(new EventDispatcher(), 'http://example.com/', sys_get_temp_dir());
         $this->assertInstanceOf('Orbt\ResourceMirror\ResourceMirror', $mirror);
     }
 
@@ -21,7 +21,16 @@ class ResourceMirrorTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetBaseUrl()
     {
-        $mirror = new ResourceMirror(new EventDispatcher(), 'http://example.com/');
+        $mirror = new ResourceMirror(new EventDispatcher(), 'http://example.com/', sys_get_temp_dir());
         $this->assertEquals('http://example.com/', $mirror->getBaseUrl());
+    }
+
+    /**
+     * A resource mirror returns the directory.
+     */
+    public function testGetDirectory()
+    {
+        $mirror = new ResourceMirror(new EventDispatcher(), 'http://example.com/', $tempDir = sys_get_temp_dir());
+        $this->assertEquals($tempDir, $mirror->getDirectory());
     }
 }
