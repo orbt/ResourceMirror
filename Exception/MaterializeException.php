b69d0a95c415435de6821f459d2d77886e17671b
Taihao Zhang <jason@zth.me>
2012-10-29 10:21:35 +1100

    Implement resource materialization.

---
+++ Exception/MaterializeException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Orbt\ResourceMirror\Exception;
+
+/**
+ * Resource exception while materializing.
+ */
+class MaterializeException extends \Exception {}
--- ResourceMirror.php
+++ ResourceMirror.php
@@ -3,6 +3,10 @@
 namespace Orbt\ResourceMirror;
 
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Orbt\ResourceMirror\Resource\Collection;
+use Orbt\ResourceMirror\Exception\MaterializeException;
+use Orbt\ResourceMirror\Resource\FileReplicator;
+use Orbt\ResourceMirror\Resource\MaterializedResource;
 
 /**
  * Main service for materializing services.
@@ -28,6 +32,12 @@ class ResourceMirror
     protected $directory;
 
     /**
+     * Resource replicator.
+     * @var Resource\Replicator
+     */
+    protected $replicator;
+
+    /**
      * Creates a new resource mirror.
      *
      * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $dispatcher
@@ -43,12 +53,12 @@ class ResourceMirror
     public function __construct(EventDispatcherInterface $dispatcher, $baseUrl, $directory)
     {
         $this->dispatcher = $dispatcher;
-        $this->baseUrl = rtrim($baseUrl, '\\/').'/';
+        $this->baseUrl = $baseUrl;
 
         if (!is_dir($directory) || !is_writable($directory)) {
             throw new \InvalidArgumentException('Directory is not writable.');
         }
-        $this->directory = $directory;
+        $this->directory = rtrim($directory, '\\/');
     }
 
     /**
@@ -67,5 +77,88 @@ class ResourceMirror
     public function getDirectory()
     {
         return $this->directory;
+    }
+
+    /**
+     * Sets a resource replicator to use when materializing.
+     * @param Resource\Replicator $replicator
+     */
+    public function setReplicator($replicator)
+    {
+        $this->replicator = $replicator;
+    }
+
+    /**
+     * Returns the replicator used.
+     * @return Resource\Replicator
+     */
+    public function getReplicator()
+    {
+        if (!isset($this->replicator)) {
+            $this->replicator = $this->createReplicator();
+        }
+        return $this->replicator;
+    }
+
+    /**
+     * Creates a replicator.
+     */
+    protected function createReplicator()
+    {
+        return new FileReplicator($this->baseUrl, $this->directory);
+    }
+
+    /**
+     * Materializes a collection of resources.
+     *
+     * @param Collection $collection
+     *   Resource collection to materialize.
+     * @return Collection
+     *   A collection of materialized resources.
+     */
+    public function materializeCollection($collection)
+    {
+        $materializedCollection = new Collection();
+        foreach ($collection as $resource) {
+            $materializedCollection->add($this->materialize($resource));
+        }
+        return $materializedCollection;
+    }
+
+    /**
+     * Materializes a resource.
+     *
+     * @param Resource\Resource $resource
+     *   Resource to materialize.
+     * @return Resource\Materialized
+     *   Materialized resource container, or the given resource if already materialized.
+     *
+     * @throws MaterializeException
+     *   If the resource cannot be materialized.
+     */
+    public function materialize($resource, $overwrite = false)
+    {
+        if ($resource instanceof MaterializedResource) {
+            return $resource;
+        }
+        if (substr($resource->getPath(), 0, 2) == '..') {
+            throw new MaterializeException('Resource is outside of base URL and directory.');
+        }
+
+        if ($overwrite || !$this->exists($resource)) {
+            $this->getReplicator()->replicate($resource);
+        }
+        return new MaterializedResource($resource, $this->getDirectory());
+    }
+
+    /**
+     * Returns whether the specified resource already exists.
+     *
+     * @param Resource\Resource $resource
+     * @return bool
+     */
+    public function exists($resource)
+    {
+        return file_exists($this->getDirectory().'/'.$resource->getPath());
     }
 }
