65c4532781944a505ddcc8432275ac08fb956be7
Taihao Zhang <jason@zth.me>
2012-10-29 08:31:40 +1100

    Test path exception for generic resource.

--- Tests/Resource/GenericResourceTest.php
+++ Tests/Resource/GenericResourceTest.php
@@ -16,6 +16,36 @@ class GenericResourceTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * A generic resource is not created for an empty path.
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCreateEmptyPath()
+    {
+        new GenericResource('');
+    }
+
+    /**
+     * A generic resource is not created for an absolute path.
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCreateAbsolutePath()
+    {
+        new GenericResource('/path');
+    }
+
+    /**
+     * A generic resource is not created for denormalized path.
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCreateDenormalizedPath()
+    {
+        new GenericResource('a/../b');
+    }
+
+    /**
      * A generic resource returns the resource path.
      *
      * @depends testCreate
