54e748d83dba0564e378a5434014090e5efcb88d
Taihao Zhang <jason@zth.me>
2012-10-28 23:17:12 +1100

    Test creating a mirror with a bad directory.

--- Tests/ResourceMirrorTest.php
+++ Tests/ResourceMirrorTest.php
@@ -37,4 +37,15 @@ class ResourceMirrorTest extends \PHPUnit_Framework_TestCase
         $mirror = new ResourceMirror(new EventDispatcher(), 'http://example.com/', $tempDir = sys_get_temp_dir());
         $this->assertEquals($tempDir, $mirror->getDirectory());
     }
+
+    /**
+     * A resource mirror is not created if directory is not writable.
+     *
+     * @depends testCreate
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCreateBadDirectory()
+    {
+        new ResourceMirror(new EventDispatcher(), 'http://example.com/', '/a/probably/not/writable/directory');
+    }
 }
