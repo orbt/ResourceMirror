8448876183d9c0f587df6fc9549e377527509653
Taihao Zhang <jason@zth.me>
2012-10-28 23:09:13 +1100

    Add directory to mirror.

--- ResourceMirror.php
+++ ResourceMirror.php
@@ -22,17 +22,33 @@ class ResourceMirror
     protected $baseUrl;
 
     /**
+     * Base resource directory.
+     * @var string
+     */
+    protected $directory;
+
+    /**
      * Creates a new resource mirror.
      *
      * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $dispatcher
      *   Event dispatcher.
      * @param string $baseUrl
      *   Base URL for the backend.
+     * @param string $directory
+     *   Path to the base directory to materialize resources to.
+     *
+     * @throws \InvalidArgumentException
+     *   If the given directory is not writable.
      */
-    public function __construct(EventDispatcherInterface $dispatcher, $baseUrl)
+    public function __construct(EventDispatcherInterface $dispatcher, $baseUrl, $directory)
     {
         $this->dispatcher = $dispatcher;
         $this->baseUrl = $baseUrl;
+
+        if (!is_dir($directory) || !is_writable($directory)) {
+            throw new \InvalidArgumentException('Directory is not writable.');
+        }
+        $this->directory = $directory;
     }
 
     /**
@@ -42,5 +58,14 @@ class ResourceMirror
     public function getBaseUrl()
     {
         return $this->baseUrl;
+    }
+
+    /**
+     * Returns the base directory used by this mirror.
+     * @return string
+     */
+    public function getDirectory()
+    {
+        return $this->directory;
     }
 }
