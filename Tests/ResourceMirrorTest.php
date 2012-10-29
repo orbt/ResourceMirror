1a607272aca1bae6df40075974ba195860ea366c
Taihao Zhang <jason@zth.me>
2012-10-29 11:05:01 +1100

    Test resource replicator and existence check.

--- Tests/ResourceMirrorTest.php
+++ Tests/ResourceMirrorTest.php
@@ -3,6 +3,8 @@
 namespace Orbt\ResourceMirror\Tests;
 
 use Orbt\ResourceMirror\ResourceMirror;
+use Orbt\ResourceMirror\Resource\GenericResource;
+use Orbt\ResourceMirror\Resource\FileReplicator;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class ResourceMirrorTest extends \PHPUnit_Framework_TestCase
@@ -47,5 +49,55 @@ class ResourceMirrorTest extends \PHPUnit_Framework_TestCase
     public function testCreateBadDirectory()
     {
         new ResourceMirror(new EventDispatcher(), 'http://example.com/', '/a/probably/not/writable/directory');
+    }
+
+    /**
+     * A resource mirror returns an automatically created replicator.
+     *
+     * @depends testCreate
+     */
+    public function testGetReplicator()
+    {
+        $mirror = new ResourceMirror(new EventDispatcher(), 'http://example.com/', sys_get_temp_dir());
+        $replicator = $mirror->getReplicator();
+        $this->assertInstanceOf('Orbt\ResourceMirror\Resource\Replicator', $replicator);
+    }
+
+    /**
+     * A resource mirror can use a different replicator.
+     *
+     * @depends testGetReplicator
+     */
+    public function testSetReplicator()
+    {
+        $mirror = new ResourceMirror(new EventDispatcher(), 'http://example.com/', sys_get_temp_dir());
+        $replicator = $mirror->getReplicator();
+        $mirror->setReplicator(new FileReplicator('http://different.com/', sys_get_temp_dir()));
+        $this->assertNotEquals($replicator, $mirror->getReplicator());
+    }
+
+    /**
+     * A resource mirror determines whether a resource exists.
+     *
+     * @depends testCreate
+     */
+    public function testExists()
+    {
+        $directory = $this->generateDirectory();
+        $resource = new GenericResource('test');
+        $mirror = new ResourceMirror(new EventDispatcher(), 'http://example.com/', $directory);
+        $this->assertFalse($mirror->exists($resource));
+        touch($directory.'/test');
+        $this->assertTrue($mirror->exists($resource));
+    }
+
+    protected function generateDirectory()
+    {
+        do {
+            $directory = sys_get_temp_dir().'/'.uniqid('FileReplicator');
+        }
+        while (file_exists($directory));
+        mkdir($directory);
+        return $directory;
     }
 }
